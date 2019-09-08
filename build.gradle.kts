0d22e32f9b4384a8eb918185f55fedbfc98b04e2
Nicholas Christopher <nwillc@gmail.com>
2019-09-08 18:03:17 -0400

    genesis

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.50"
+    id("com.palantir.docker-run") version "0.22.1"
+    id("com.palantir.docker") version "0.22.1"
+}
+
+group = "com.github.nwillc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+docker {
+    name = "multi-db:1.0"
+    files("src/main/docker/postgresql/create-databases.sh")
+    setDockerfile(File("src/main/docker/postgresql/Dockerfile"))
+}
+
+dockerRun {
+    name = "postgresql"
+    image = "multi-db:1.0"
+    ports("5432:5432")
+    env(mutableMapOf(
+        "POSTGRES_PASSWORD" to "example",
+        "POSTGRES_USER" to "postgres",
+        "POSTGRES_MULTIPLE_DATABASES" to "payment-service party-service"
+    ))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
